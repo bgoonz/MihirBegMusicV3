acec7e3a27e287a38f765fe45a4ad3e2d6f53978
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 09:47:43 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,36 +44,7 @@ export default class Footer extends React.Component {
         return (
             <footer className="site-footer">
 
-              {/* start of freefind search box html */}
-      <table cellPadding={0} cellSpacing={0} border={0}>
-        <tbody><tr>
-            <td style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt'}}>
-              <center><table width="90%" cellPadding={0} cellSpacing={0} border={0} style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt'}}>
-                  <tbody><tr>
-                      <td style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt'}} align="left"><a href="https://search.freefind.com/siteindex.html?si=10862521">index</a></td>
-                      <td style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt'}} align="center"><a href="https://search.freefind.com/find.html?si=10862521&m=0&p=0">sitemap</a></td>
-                      <td style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt'}} align="right"><a href="https://search.freefind.com/find.html?si=10862521&pid=a">advanced</a></td>
-                    </tr>
-                  </tbody></table></center>
-              <form style={{margin: '0px', marginTop: '4px'}} action="https://search.freefind.com/find.html" method="get" acceptCharset="utf-8" target="_self">
-                <input type="hidden" name="si" defaultValue={10862521} />
-                <input type="hidden" name="pid" defaultValue="r" />
-                <input type="hidden" name="n" defaultValue={0} />
-                <input type="hidden" name="_charset_" defaultValue />
-                <input type="hidden" name="bcd" defaultValue="÷" />
-                <input type="text" name="query" size={15} /> 
-                <input type="submit" defaultValue="search" />
-              </form>
-            </td>
-          </tr>
-          <tr>
-            <td style={{textAlign: 'center', fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt', paddingTop: '4px'}}>
-              <a style={{textDecoration: 'none', color: 'gray'}} href="https://www.freefind.com" rel="nofollow">search engine</a><a style={{textDecoration: 'none', color: 'gray'}} href="https://www.freefind.com" rel="nofollow"> by
-                <span style={{color: '#606060'}}>freefind</span></a>
-            </td>
-          </tr>
-        </tbody></table>
-      {/* end of freefind search box html */}
+            
                 {(logo || (hasPrimaryNav && !_.isEmpty(primaryNavLinks)) || (hasSecondaryNav && !_.isEmpty(secondaryNavLinks)) || (hasTertiaryNav && !_.isEmpty(tertiaryNavLinks))) && (
                     <div className="site-footer__nav py-5 py-md-6">
                         <div className="container">
